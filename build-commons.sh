85f21eedb5ca605e25674140b3802c97ea278357
mpyw <ryosuke_i_628@yahoo.co.jp>
2015-11-25 14:47:52 +0900

    Added minify build option

--- build-commons.sh
+++ build-commons.sh
@@ -4,5 +4,5 @@ oslist=(darwin windows linux freebsd)
 for os in "${oslist[@]}"; do
     [ "$os" = "windows" ] && suffix=".exe" || suffix=""
     dist="dist/twhelp-x64${os}${suffix}"
-    $(GOARCH=amd64 GOOS="$os" go build -o "$dist" && zip "$dist.zip" "$dist") &
+    $(GOARCH=amd64 GOOS="$os" go build -ldflags="-s -w" -o "$dist" && zip "$dist.zip" "$dist") &
 done
