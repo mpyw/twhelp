b752d8ae1a3cb49899b51413c181896834fadd50
mpyw <ryosuke_i_628@yahoo.co.jp>
2016-11-01 07:42:39 +0900

    Update build script to use docker

--- build-commons.sh
+++ build-commons.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-oslist=(darwin windows linux freebsd)
-for os in "${oslist[@]}"; do
-    [ "$os" = "windows" ] && suffix=".exe" || suffix=""
-    dirname="dist/$os"
-    filename="twhelp$suffix"
-    zipname="../twhelp-x64$os.zip"
-    (
-        GOARCH=amd64 GOOS="$os" go build -ldflags="-s -w" -o "$dirname/$filename" &&
-        cd "$dirname" &&
-        zip "$zipname" "$filename"
-    ) &
-done
-wait
+docker run --rm -it -v "$PWD":/_ -w /_ golang:1.7 bash -c '
+
+    go get -d ./...
+
+    oslist=(darwin windows linux freebsd)
+    for os in "${oslist[@]}"; do
+        [ "$os" = "windows" ] && suffix=".exe" || suffix=""
+        dirname="dist/$os"
+        filename="twhelp$suffix"
+        (GOARCH=amd64 GOOS="$os" go build -ldflags="-s -w" -o "$dirname/$filename") &
+    done
+
+    wait
+
+' && {
+
+    oslist=(darwin windows linux freebsd)
+    for os in "${oslist[@]}"; do
+        [ "$os" = "windows" ] && suffix=".exe" || suffix=""
+        dirname="dist/$os"
+        filename="twhelp$suffix"
+        zipname="../twhelp-x64$os.zip"
+        (cd "$dirname" && zip "$zipname" "$filename") &
+    done
+
+    wait
+
+}
