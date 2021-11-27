725a03f97b9b587e733d89a748d233d626eec46f
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-11-27 15:32:57 +0900

    feat: 🎸 Support arm64 architecture

--- build-commons.sh
+++ build-commons.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-docker run --rm -it -v "$PWD":/_ -w /_ golang:1.13 bash -c '
+docker run --rm -it -v "$PWD":/_ -w /_ golang:1.17 bash -c '
 
-    go get -d ./...
+    GO111MODULE=off go get -d ./...
 
     oslist=(darwin windows linux freebsd)
     for os in "${oslist[@]}"; do
-        [ "$os" = "windows" ] && suffix=".exe" || suffix=""
-        dirname="dist/$os"
+        [ "$os" = windows ] && suffix=.exe || suffix=
+        dirname="dist/$os/amd64"
         filename="twhelp$suffix"
-        (GOARCH=amd64 GOOS="$os" go build -ldflags="-s -w" -o "$dirname/$filename") &
+        (GO111MODULE=off GOARCH=amd64 GOOS="$os" go build -ldflags="-s -w" -o "$dirname/$filename") &
+        if [ "$os" = darwin ]; then
+            (GO111MODULE=off GOARCH=arm64 GOOS=darwin go build -ldflags="-s -w" -o dist/darwin/arm64/twhelp) &
+        fi
     done
 
     wait
@@ -18,11 +21,14 @@ docker run --rm -it -v "$PWD":/_ -w /_ golang:1.13 bash -c '
 
     oslist=(darwin windows linux freebsd)
     for os in "${oslist[@]}"; do
-        [ "$os" = "windows" ] && suffix=".exe" || suffix=""
-        dirname="dist/$os"
+        [ "$os" = windows ] && suffix=.exe || suffix=
+        dirname="dist/$os/amd64"
         filename="twhelp$suffix"
-        zipname="../twhelp-x64$os-$1.zip"
+        zipname="../../twhelp-$os-amd64-$1.zip"
         (cd "$dirname" && zip "$zipname" "$filename") &
+        if [ "$os" = darwin ]; then
+            (cd dist/darwin/arm64 && zip "../../twhelp-darwin-arm64-$1.zip" "twhelp") &
+        fi
     done
 
     wait
